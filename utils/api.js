418dec43cf5419780228a1ffbff54527ea558e5d
Adil Ashurov <ashurovadil96@gmail.com>
2025-06-27 01:29:22 +0400

    add new link for api

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://test.aquadaddy.com/";
+  process.env.NEXT_PUBLIC_API_URL || "https://test.aquadaddy.app/";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
