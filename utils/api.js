e8b11e6d6742eda1eaf115d1ba17ba4801a6c59b
Adil Ashurov <ashurovadil96@gmail.com>
2025-06-27 00:11:51 +0400

    add ip

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://194.163.151.112:3005/";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
